05736979662b4d508a8015f97ff46fccfd76d3e6
szabolcshlatky <szabolcshlatky@gmail.com>
2023-09-06 16:48:26 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+"use strict";
+var $ = function (id) { return document.getElementById(id); };
+var $$ = function (query) { return document.querySelector(query); };
+var $$$ = function (jquery) { return document.querySelectorAll(jquery); };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2NyaXB0LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsic2NyaXB0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLFlBQVksQ0FBQztBQUViLElBQU0sQ0FBQyxHQUFHLFVBQUMsRUFBRSxJQUFLLE9BQUEsUUFBUSxDQUFDLGNBQWMsQ0FBQyxFQUFFLENBQUMsRUFBM0IsQ0FBMkIsQ0FBQztBQUM5QyxJQUFNLEVBQUUsR0FBRyxVQUFDLEtBQUssSUFBSyxPQUFBLFFBQVEsQ0FBQyxhQUFhLENBQUMsS0FBSyxDQUFDLEVBQTdCLENBQTZCLENBQUM7QUFDcEQsSUFBTSxHQUFHLEdBQUcsVUFBQyxNQUFNLElBQUssT0FBQSxRQUFRLENBQUMsZ0JBQWdCLENBQUMsTUFBTSxDQUFDLEVBQWpDLENBQWlDLENBQUMifQ==
---
+++ scripts/script.ts
@@ -0,0 +1,5 @@
+`use strict`;
+
+const $ = (id) => document.getElementById(id);
+const $$ = (query) => document.querySelector(query);
+const $$$ = (jquery) => document.querySelectorAll(jquery);
